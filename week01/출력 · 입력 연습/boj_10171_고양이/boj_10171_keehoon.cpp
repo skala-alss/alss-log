03489a02d1acfc92b6ad3b9235bc1e3eb3b7a8ea
Won, Kee-hoon <amorinael@gmail.com>
2025-08-02 22:19:29 +0900

    2508022219

---
+++ week01/출력 · 입력 연습/boj_10171_고양이/boj_10171_keehoon.cpp
@@ -0,0 +1,15 @@
+// 고양이 (10171)
+// https://www.acmicpc.net/problem/10171
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\\    /\\" << '\n';
+    cout << " )  ( ')" << '\n';
+    cout << "(  /  )" << '\n';
+    cout << " \\(__)|" << '\n';
+    return 0;
+}
