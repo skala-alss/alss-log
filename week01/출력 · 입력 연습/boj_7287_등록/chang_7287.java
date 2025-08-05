a6d1c99beb1bfb9401a5e79abe84436d3b453af4
skala-changhyun <gcgc09@naver.com>
2025-08-05 18:23:05 +0900

    0805

--- week01/출력 · 입력 연습/boj_7287_등록/chang_7287.java
+++ week01/출력 · 입력 연습/boj_7287_등록/chang_7287.java
@@ -1,6 +1,6 @@
 public class chang_7287 {
     public static void main(String[] args) {
-        System.out.println("3");
+        System.out.println("6");
         System.out.println("gcgc09");
     }
 
