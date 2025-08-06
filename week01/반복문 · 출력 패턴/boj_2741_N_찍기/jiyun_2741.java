f594670d9c2de35ddb341a1baad7c3ca2fa434e9
송지윤 <jiyun301@naver.com>
2025-08-06 11:48:50 +0900

    sol.2741 0806

---
+++ week01/반복문 · 출력 패턴/boj_2741_N_찍기/jiyun_2741.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class jiyun_2741 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
