ab1eae7166a77c3901577f81468bf4f481c31304
Tony <sdgeo2020@gmail.com>
2025-08-07 16:03:50 +0900

    250807 1600

---
+++ week01/문자열/boj_11654_아스키_코드/donggeon_11654.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class donggeon_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+        System.out.println((int) c);
+    }
+}
