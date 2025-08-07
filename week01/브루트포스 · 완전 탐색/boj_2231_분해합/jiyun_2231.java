7735a82c26b8598ca87412dff5734a5c58c7fce7
송지윤 <jiyun301@naver.com>
2025-08-07 10:19:36 +0900

    sol.2231 0807

--- week01/브루트포스 · 완전 탐색/boj_2231_분해합/jiyun_2231.java
+++ week01/브루트포스 · 완전 탐색/boj_2231_분해합/jiyun_2231.java
@@ -4,24 +4,24 @@ public class jiyun_2231 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        int result = 0;
         // n = (100*a + 10*b + 1*c) + a + b + c
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n+1;i++){
 
             int temp=i;
             int sum=0;
 
             while(temp != 0){
                 sum += temp % 10;
-                temp = temp / 10;
+                temp /= 10;
             }
 
-            if(i +sum ==n){
-                System.out.println(i);
+            if(i + sum == n){
+                result = i;
                 break;
             }
         }
-
+        System.out.println(result);
     }
 }
 
