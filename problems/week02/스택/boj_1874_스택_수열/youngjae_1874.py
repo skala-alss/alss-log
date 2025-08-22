b077816ea4a59143a11d1cdb73755c01a90396e9
seo-youngjae <heaven_707@naver.com>
2025-08-22 10:10:45 +0900

    250822

--- problems/week02/스택/boj_1874_스택_수열/youngjae_1874.py
+++ problems/week02/스택/boj_1874_스택_수열/youngjae_1874.py
@@ -23,7 +23,8 @@ def main() -> None:
         
         # print(block)
         stack, ans = [], []
-        for i in range
+        for i in range(n):
+            
     
     
     # print(stack)
@@ -34,3 +35,40 @@ def main() -> None:
 if __name__ == "__main__":
     main()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+
+    merged_arr = []
+    l = h = 0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] < high_arr[h]:
+            merged_arr.append(low_arr[l])
+            l += 1
+        else:
+            merged_arr.append(high_arr[h])
+            h += 1
+    merged_arr += low_arr[l:]
+    merged_arr += high_arr[h:]
+    return merged_arr
