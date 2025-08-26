1b1ae2c03adc621a5433bff50250c4fc84601a4b
skala-changhyun <gcgc09@naver.com>
2025-08-26 21:48:21 +0900

    0826

---
+++ problems/week01/문자열/boj_14935_FA/chang_14935.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class chang_14935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = br.readLine().trim();
+        int a = n.charAt(0) - '0';
+        int b = n.length();
+
+        System.out.println("FA");
+    }
+
+}
