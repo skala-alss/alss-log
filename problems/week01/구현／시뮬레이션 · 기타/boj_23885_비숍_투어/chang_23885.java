941c4e45958864ca556cfb97f1f2d4c51dffdd5a
skala-changhyun <gcgc09@naver.com>
2025-09-04 18:04:13 +0900

    0904

--- problems/week01/구현／시뮬레이션 · 기타/boj_23885_비숍_투어/chang_23885.java
+++ problems/week01/구현／시뮬레이션 · 기타/boj_23885_비숍_투어/chang_23885.java
@@ -1,4 +1,26 @@
 import java.io.*;
 import java.util.*;
+
 public class chang_23885 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        if ((x1 + y1) % 2 == (x2 + y2) % 2) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
 }
