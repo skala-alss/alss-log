aae537ed7d1d220b1143a60efd85eac6a2dc3336
송지윤 <jiyun301@naver.com>
2025-09-04 12:40:10 +0900

    solv.23882 0903

---
+++ problems/week05/정렬/boj_23882_알고리즘_수업_-_선택_정렬_2/jiyun_23882.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class jiyun_23882 {
+    static int[] arr;
+
+    static void selectionSort(int[] arr, int n, int k){
+        int count = 0;
+        for (int i = n - 1; i > 0; i--) {
+            int maxIndex = i;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[maxIndex]) maxIndex = j;
+            }
+            if (maxIndex == i) continue;
+
+            swap(arr, maxIndex, i);
+            count++;
+
+            if (count == k) {
+                break;
+            }
+        }
+        if (count < k) {
+            System.out.println(-1);
+        } else {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%d ", arr[j]);
+            }
+        }
+    }
+
+
+    static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        selectionSort(arr,N,K);
+    }
+
+}
