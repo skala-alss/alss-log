c922712c53e3a60bdee48024c5aaaece971e45fe
Won, Kee-hoon <amorinael@gmail.com>
2025-10-26 11:54:01 +0900

    2510261153

--- problems/week04/브루트포스&재귀&백트래킹-고급/boj_30242_N_Queen_(Easy)/keehoon_30242.cpp
+++ problems/week04/브루트포스&재귀&백트래킹-고급/boj_30242_N_Queen_(Easy)/keehoon_30242.cpp
@@ -39,8 +39,56 @@ int main()
 #endif
 
   int N;
-  vector<int> cols(N);
-  for (int &col : cols)
-    cin >> col;
+  cin >> N;
+  vector<int> rows(N), cols(N, 0), diag1(N * 2, 0), diag2(N * 2, 0);
+  for (int &row : rows)
+    cin >> row;
+
+  for (int row = 0; row < N; ++row)
+    if (rows[row])
+    {
+      int col = rows[row] - 1;
+      cols[col] = 1;
+      diag1[row - col + N - 1] = 1;
+      diag2[row + col] = 1;
+    }
+
+  vector<int> result(N, 0);
+  bool found = false;
+  auto backtracking = [&](auto &&self, int row) -> void
+  {
+    if (found)
+      return;
+
+    if (row == N)
+    {
+      result = rows;
+      found = true;
+      return;
+    }
+
+    if (rows[row])
+      self(self, row + 1);
+
+    if (!rows[row])
+      for (int col = 0; col < N; ++col)
+        if (!cols[col] && !diag1[row - col + N - 1] && !diag2[row + col])
+        {
+          rows[row] = col + 1;
+          cols[col] = diag1[row - col + N - 1] = diag2[row + col] = 1;
+          self(self, row + 1);
+          rows[row] = 0;
+          cols[col] = diag1[row - col + N - 1] = diag2[row + col] = 0;
+        }
+  };
+
+  backtracking(backtracking, 0);
+
+  if (!found)
+    cout << -1;
+  else
+    for (int &col : result)
+      cout << col << ' ';
+
   return 0;
 }
