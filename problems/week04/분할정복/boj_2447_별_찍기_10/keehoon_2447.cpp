6596cfddd184a8ebb92124f59e3ba24a07cfea2c
WON, KEEHOON <lasnier@naver.com>
2025-12-02 22:34:50 +0900

    2512022234

--- problems/week04/분할정복/boj_2447_별_찍기_10/keehoon_2447.cpp
+++ problems/week04/분할정복/boj_2447_별_찍기_10/keehoon_2447.cpp
@@ -10,29 +10,34 @@
 
 #include <iostream>
 #if defined(_WIN32)
-  #include <io.h>
-  #include <cstdio>
+#include <io.h>
+#include <cstdio>
 #else
-  #include <unistd.h>
-  #include <cstdio>
+#include <unistd.h>
+#include <cstdio>
 #endif
 
 using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    // 표준입력이 터미널이면 input.txt로 대체 (파이프 입력 시에는 그대로 cin)
+  // 표준입력이 터미널이면 input.txt로 대체 (파이프 입력 시에는 그대로 cin)
 #if defined(_WIN32)
-    if (_isatty(_fileno(stdin))) { freopen("input.txt", "r", stdin); }
+  if (_isatty(_fileno(stdin)))
+  {
+    freopen("input.txt", "r", stdin);
+  }
 #else
-    if (isatty(fileno(stdin))) { freopen("input.txt", "r", stdin); }
+  if (isatty(fileno(stdin)))
+  {
+    freopen("input.txt", "r", stdin);
+  }
 #endif
 
-    int N, M;
-    if (!(cin >> N >> M)) return 0;
-    cout << "N: " << N << " M: " << M << "\n";
-    return 0;
+  int N;
+  cin >> N;
+  return 0;
 }
